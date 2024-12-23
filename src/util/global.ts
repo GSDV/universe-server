b813066982143384f93cf58de5f217f40abf485f
GSDV <sdvgab@gmail.com>
2024-12-22 19:51:27 -0800

    pfpKeyPrefix

--- src/util/global.ts
+++ src/util/global.ts
@@ -22,6 +22,14 @@ export const ACCOUNT_POSTS_PER_BATCH = 15;
 // USER RESTRICTIONS
 export const MIN_USERNAME_LENGTH = 4;
 export const MAX_USERNAME_LENGTH = 25;
+export const isValidUsername = (input: string) => {
+    const pattern = /^[a-z][a-z0-9_]+$/;
+    return input.length > 3 && input.length <= 20 && input[0] != '.' && input[input.length-1] != '.' && pattern.test(input);
+}
+
+export const MIN_DISPLAY_NAME_LENGTH = 1;
+export const MAX_DISPLAY_NAME_LENGTH = 30;
+export const isValidDisplayName = (input: string) => (input.length > 0 && input.length <= 30);
 
 
 
@@ -38,8 +46,11 @@ export const IMG_SIZE_LIMIT_TXT = `10mb`;
 export const VID_SIZE_LIMIT = 50 * 1000000;
 export const VID_SIZE_LIMIT_TXT = `50mb`;
 
+export const MAX_REPORT_LENGTH = 250;
 
 
-// Media Links
+
+// AWS S3 Links
+export const pfpKeyPrefix = (authorId: string) => `users/${authorId}/avatar`;
 export const mediaKeyPrefix = (authorId: string) => `users/${authorId}/posts/`;
 export const mediaUrl = (mediaKey: string) => `https://uni-verse.s3.us-east-2.amazonaws.com/${mediaKey}`;
