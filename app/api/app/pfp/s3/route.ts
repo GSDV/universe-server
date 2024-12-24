5520a0a565f6a390d0a9939e29caaebbb2912262
GSDV <sdvgab@gmail.com>
2024-12-23 17:18:14 -0800

    Removing avatar-specific s3 link creation

--- app/api/app/pfp/s3/route.ts
+++ app/api/app/pfp/s3/route.ts
@@ -2,10 +2,10 @@ import { NextRequest } from 'next/server';
 
 import { getValidatedUser } from '@util/prisma/actions/user';
 
-import { ACCEPTED_IMGS, AVATAR_SIZE_LIMIT_TXT, AVATAR_SIZE_LIMIT, userPfpKey } from '@util/global';
+import { ACCEPTED_IMGS, AVATAR_SIZE_LIMIT_TXT, AVATAR_SIZE_LIMIT, avatarKeyPrefix } from '@util/global';
 import { response } from '@util/global-server';
 
-import { deleteFromS3, getSignedS3PfpUrl } from '@util/aws';
+import { deleteFromS3, getSignedS3Url } from '@util/aws';
 
 
 
@@ -21,10 +21,11 @@ export async function POST(req: NextRequest) {
         if (!ACCEPTED_IMGS.includes(fileType)) return response(`Upload only png, jpg, or webp files.`, 102);
         if (fileSize > AVATAR_SIZE_LIMIT) return response(`Upload images less than ${AVATAR_SIZE_LIMIT_TXT}.`, 102);
 
-        const pfpKey = userPfpKey(userPrisma.id);
-        await deleteFromS3(pfpKey);
+        await deleteFromS3(userPrisma.pfpKey);
 
-        const { signedUrl, key } = await getSignedS3PfpUrl(pfpKey, fileType);
+        const avatarPrefix = avatarKeyPrefix(userPrisma.id);
+        const { signedUrl, key } = await getSignedS3Url(avatarPrefix, fileType);
+
         return response(`Success`, 200, { signedUrl, key });
     } catch (err) {
         return response(`Server error: ${err}`, 900);
