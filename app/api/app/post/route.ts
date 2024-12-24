834335cda47d3ca4a7c95972261e45f095df440d
GSDV <sdvgab@gmail.com>
2024-12-23 18:14:45 -0800

    Fixed checking for mediaKey through prefix "startsWith"

--- app/api/app/post/route.ts
+++ app/api/app/post/route.ts
@@ -1,14 +1,11 @@
 import { NextRequest } from 'next/server';
 
-import { cookies } from 'next/headers';
-
-import { getRedactedUserFromAuth } from '@util/prisma/actions/user';
+import { getValidatedUser } from '@util/prisma/actions/user';
 import { createPost } from '@util/prisma/actions/posts';
 
-import { AUTH_TOKEN_COOKIE_KEY } from '@util/global';
+import { mediaKeyPrefix } from '@util/global';
 import { response } from '@util/global-server';
 
-import { isValidUser } from '@util/api/user';
 import { validateCreatePostData } from '@util/api/posts';
 
 
@@ -19,21 +16,17 @@ export async function POST(req: NextRequest) {
         const { postDataInput } = await req.json();
         if (!postDataInput) return response(`No data provided.`, 101);
 
-        const cookieStore = await cookies();
-        const authTokenCookie = cookieStore.get(AUTH_TOKEN_COOKIE_KEY);
-        if (!authTokenCookie) return response(`You are not logged in.`, 103);
-        const authToken = authTokenCookie.value;
-        const userPrisma = await getRedactedUserFromAuth(authToken);
-        if (!userPrisma) return response(`You are not logged in`, 404);
-        const resValidUser = isValidUser(userPrisma);
-        if (!resValidUser.valid) return resValidUser.res;
+        const { userPrisma, validUserResp } = await getValidatedUser();
+        if (!userPrisma) return validUserResp;
 
         const resInput = validateCreatePostData(postDataInput);
         if (!resInput.valid || resInput.data == undefined) return resInput.resp;
         const postData = resInput.data;
 
         // Make sure every media key passed in comes from the user posting.
-        const postIncludesOtherKey = postData.media.some((key) => !(key.includes(userPrisma.id)));
+        const mediaPrefix = mediaKeyPrefix(userPrisma.id);
+        const postIncludesOtherKey = postData.media.some((key) => !key.startsWith(mediaPrefix));
+
         if (postIncludesOtherKey) return response('Something went wrong.', 101);
 
         const post = await createPost(userPrisma.id, postData);
