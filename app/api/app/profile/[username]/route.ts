ef868f698cd64d8b8cfed8d6d20f847278666a85
GSDV <sdvgab@gmail.com>
2025-01-02 13:59:29 -0800

    Finding and returning isFollowed field

---
+++ app/api/app/profile/[username]/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest } from 'next/server';
+
+import { getUserWithUniAndFollows, getValidatedUser } from '@util/prisma/actions/user';
+
+import { response } from '@util/global-server';
+
+import { redactUserPrisma } from '@util/api/user';
+
+
+
+// Used to fetch the profile info of another account when viewing.
+export async function GET(req: NextRequest, { params }: { params: Promise<{ username: string }> }) {
+    try {
+        const username = (await params).username;
+
+        const { userPrisma: loggedInUserPrisma } = await getValidatedUser();
+        const loggedInId= (!loggedInUserPrisma) ? '' : loggedInUserPrisma.id;
+
+        const userPrisma = await getUserWithUniAndFollows({ username }, loggedInId);
+        if (!userPrisma) return response(`Account does not exist.`, 404);
+
+        const redactedUserPrisma = redactUserPrisma(userPrisma);
+        const clientUserPrisma = {
+            ...redactedUserPrisma,
+            isFollowed: userPrisma.isFollowed
+        }
+
+        const ownAccount = clientUserPrisma.id === loggedInId;
+
+        return response(`Success`, 200, { user: clientUserPrisma, ownAccount });
+    } catch (err) {
+        return response(`Server error: ${err}`, 904);
+    }
+}
---
+++ src/util/global-server.ts
@@ -0,0 +1,72 @@
+import { Prisma } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+
+
+// TOKEN RESTRICTIONS
+// Maximum attempts allowed for one activation token, before it is deleted and user must request another
+export const MAX_ACTIVATE_TOKEN_ATTEMPTS = 5;
+// ActivateTokens are expired if they were sent more than an hour ago
+export const ACTIVATE_TOKEN_EXPIRATION = 60;
+
+
+
+export const POST_PER_SCROLL = 15;
+
+
+
+// UTILS FOR PRISMA CALLS
+export const OMIT_USER = {
+    createdAt: true,
+    updatedAt: true,
+    email: true,
+    password: true,
+    salt: true,
+    deleted: true,
+    banned: true,
+    banExpiration: true,
+    banMsg: true
+} satisfies Prisma.UserOmit;
+
+export const INCLUDE_AUTHOR = {
+    author: {
+        include: { university: true },
+        omit: OMIT_USER
+    }
+} satisfies Prisma.PostInclude;
+
+
+
+export const getUserLike = (userId: string) => {
+    return {
+        where: { userId },
+        select: { id: true },
+        take: 1
+    };
+}
+
+
+
+export const getUserFollow = (followerId: string) => {
+    return {
+        where: { followerId }
+    };
+}
+
+
+
+export const COUNT_REPLIES = {
+    _count: {
+        select: {
+            replies: true
+        }
+    }
+}
+
+
+
+// NextResponse used in API routes
+export const response = (msg: string, cStatus: number, data?: Record<string, unknown>) => {
+    if (cStatus/100 == 2) return NextResponse.json({ cStatus, msg, ...(data || {}) }, { status: 200 })
+    return NextResponse.json({ cStatus, msg, ...(data || {}) }, { status: 400 })
+}
