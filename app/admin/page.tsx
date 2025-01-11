23022d71d93041ba802fe3034d826797fd08d1b3
GSDV <sdvgab@gmail.com>
2025-01-10 20:07:42 -0800

    admin portal start

---
+++ app/admin/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+// import CenterLayout from '@components/containers/CenterLayout';
+// import { CheckIfAdmin, Subsection } from '@components/pages/admin/Admin';
+
+// import styles from '@styles/pages/admin.module.css';
+
+
+
+
+
+export default function Page() {
+    return (
+        <div>
+            Admin
+        </div>
+    )
+    // return (
+    //     <CenterLayout>
+    //         <CheckIfAdmin content={<Dashboard />} />
+    //     </CenterLayout>
+    // );
+}
+
+
+
+// function Dashboard() {
+//     return (
+//         <div className={styles.dashboard}>
+//             <h1>Admin Dashboard</h1>
+//             <div className={styles.actionsContainer}>
+//                 <Subsection title='Users' link='/admin/users/' />
+//                 <Subsection title='Posts' link='/admin/posts/' />
+//                 <Subsection title='Promo' link='/admin/promos/' />
+//             </div>
+//         </div>
+//     );
+// }
