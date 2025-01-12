e73d92e4f95ab0d322421b36df24176d27c8207d
GSDV <sdvgab@gmail.com>
2025-01-11 22:57:28 -0800

    Removed relative urls as they don't exist in server actions

--- src/util/fetch.ts
+++ src/util/fetch.ts
@@ -13,7 +13,7 @@ type Method = 'GET' | 'POST' | 'PUT' | 'DELETE';
 // Note: This method differs from client "fetchBasic" by needing to specify "admin" or "app" as first part in route.
 export const fetchBasic = async (route: string, method: Method, body?: string) => {
     try {
-        const res = await fetch(`/api/${API_VERSION}/${route}`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/${API_VERSION}/${route}`, {
             method,
             body,
             headers: {
@@ -23,6 +23,7 @@ export const fetchBasic = async (route: string, method: Method, body?: string) =
         const resJson = await res.json();
         return resJson;
     } catch (err) {
+        console.log(err)
         return { msg: `Something went wrong.`, cStatus: 800 };
     }
 }
@@ -35,7 +36,7 @@ export const fetchAdminWithAuth = async (route: string, method: Method, body?: s
         const authTokenCookie = cookieStore.get(ADMIN_AUTH_TOKEN_COOKIE_KEY);
         if (!authTokenCookie || authTokenCookie.value==='') return { msg: `Unathorized.`, cStatus: 100 };
 
-        const res = await fetch(`/api/${API_VERSION}/admin/${route}`, {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/${API_VERSION}/admin/${route}`, {
             method,
             body,
             headers: {
