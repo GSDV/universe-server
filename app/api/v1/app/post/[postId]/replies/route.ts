7c26b314f865dc0bd5c0dc9a24c606d98edd5db0
GSDV <sdvgab@gmail.com>
2025-01-19 13:06:36 -0800

    Updated to use cursor

--- app/api/v1/app/post/[postId]/replies/route.ts
+++ app/api/v1/app/post/[postId]/replies/route.ts
@@ -1,23 +1,37 @@
 import { NextRequest } from 'next/server';
 
+import { Prisma } from '@prisma/client';
+
 import { getValidatedUser } from '@util/prisma/actions/user';
-import { getPostWithAncestorsClient, getRepliesClient } from '@util/prisma/actions/posts';
+import { fetchClientBatchPosts } from '@util/prisma/actions/posts';
 
 import { response } from '@util/global-server';
 
 
 
-// Given a postId, fetch all the ancestors in its thread.
+// Given a postId, fetch its replies.
 export async function GET(req: NextRequest, { params }: { params: Promise<{ postId: string }> }) {
     try {
         const postId = (await params).postId;
-        // dont need validation for route excepy\t for seeing if user liked a post before
+        const { searchParams } = new URL(req.url);
+        const cursor = searchParams.get('cursor');
+        if (cursor === null) return response(`Missing cursor.`, 101);
+
         const { userPrisma } = await getValidatedUser();
         const loggedInId = (!userPrisma) ? '' : userPrisma.id;
 
-        const { replies, moreRepliesAvailable } = await getRepliesClient(postId, 1, loggedInId);
+        const where: Prisma.PostWhereInput = {
+            replyToId: postId,
+            deleted: false
+        };
+        const orderBy: Prisma.Enumerable<Prisma.PostOrderByWithRelationInput> = [
+            { likeCount: 'desc' },
+            { replyCount: 'desc' },
+            { displayDate: 'asc' }
+        ];
+        const { clientPosts, nextCursor, moreAvailable } = await fetchClientBatchPosts(where, cursor, loggedInId, orderBy);
 
-        return response(`Success`, 200, { replies, moreRepliesAvailable });
+        return response(`Success`, 200, { replies: clientPosts, nextCursor, moreAvailable });
     } catch (_) {
         return response(`Server error.`, 903);
     }
