6b8d68592e0e77da43273f369db821a181828229
GSDV <sdvgab@gmail.com>
2025-01-24 16:57:09 -0600

    Checking proper expiry

--- app/api/v1/app/password/route.ts
+++ app/api/v1/app/password/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from 'next/server';
 import { waitUntil } from '@vercel/functions';
 
 import { getUser, updateUser } from '@util/prisma/actions/user';
-import { createRPToken, deleteRPTokens, getRPToken } from '@util/prisma/actions/tokens';
+import { createRPToken, deleteAllAuthToken, deleteRPTokens, getRPToken } from '@util/prisma/actions/tokens';
 
 import { response } from '@util/global-server';
 
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest) {
         if (!isValid.valid || !userPrisma) return isValid.res;
 
         const prevRpToken = await getRPToken({ userId: userPrisma.id });
-        if (isRPTokenExpired(prevRpToken)) return response(`Please wait before requesting another reset password email.`, 510);
+        if (!isRPTokenExpired(prevRpToken)) return response(`Please wait before requesting another reset password email.`, 510);
         await deleteRPTokens({ userId: userPrisma.id });
 
         const rpToken = await createRPToken(userPrisma.id);
@@ -47,7 +47,7 @@ export async function PUT(req: NextRequest) {
         if (newPassword == '' || !isValidPassword(newPassword)) return response(`Use a password with 5 to 50 characters, consisting only of letters, numbers, and the symbols #, $, %, and &.`, 102);
 
         const rpTokenPrisma = await getRPToken({ token: rpToken });
-        if (!rpTokenPrisma) return response(`Reset password token is expired.`, 102);
+        if (!rpTokenPrisma || isRPTokenExpired(rpTokenPrisma)) return response(`Reset password token is expired.`, 102);
         const userPrisma = rpTokenPrisma.user;
         const isValid = isValidUser(userPrisma);
         if (!isValid.valid || !userPrisma) return isValid.res;
@@ -56,6 +56,7 @@ export async function PUT(req: NextRequest) {
         await updateUser({ id: userPrisma.id }, { password: hashedPassword });
 
         waitUntil(deleteRPTokens({ userId: userPrisma.id }));
+        waitUntil(deleteAllAuthToken(userPrisma.id));
 
         return response(`Success, your password has been reset.`, 200);
     } catch (err) {
