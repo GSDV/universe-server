df12c39589834c40bc9cd7605092eaa6e4d53102
GSDV <sdvgab@gmail.com>
2025-01-24 17:00:20 -0600

    Homepage coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Index() {
     return (
-        <div style={{ width: '100%', height: '100%', backgroundColor: 'white' }}>
-            <p>UniVerse</p>
+        <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+            <h4 style={{ textAlign: 'center', fontSize: '40px', fontWeight: 900, color: 'var(--primary)' }}>UniVerse</h4>
+            <p style={{ textAlign: 'center', fontSize: '20px', color: 'black' }}>Coming soon...</p>
         </div>
     );
 }
