79b177802df36fac03423362f3c001bbc9e3081a
GSDV <sdvgab@gmail.com>
2025-02-07 19:58:50 -0600

    markPostDelete decrements replyTo replyCount if needed

--- app/page.tsx
+++ app/page.tsx
@@ -6,3 +6,14 @@ export default function Index() {
         </div>
     );
 }
+
+function NewHome() {
+    return (
+        <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+            <h1 style={{ textAlign: 'center', fontSize: '40px', fontWeight: 900, color: 'var(--primary)' }}>UniVerse</h1>
+            <h5 style={{ textAlign: 'center', fontSize: '20px', color: 'black' }}>Download UniVerse:</h5>
+            {/* UniVerse App Store link here */}
+            <p style={{ textAlign: 'center', fontSize: '20px', color: 'rgb(117, 117, 117)' }}>By using UniVerse, you agree to our <a href='/terms'>terms</a> and <a href='privacy-policy'>privacy policy</a>.</p>
+        </div>
+    );
+}
