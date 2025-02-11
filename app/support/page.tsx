04d6d9c4556914d768f9697e043976a48ac6fa39
GSDV <sdvgab@gmail.com>
2025-02-11 07:50:39 -0600

    Word change

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
         <div className={styles.container}>
             <h1 style={{ color: 'var(--primary)', fontSize: '30px', fontWeight: 900 }}>Support</h1>
             <p>For all questions, please email us at <a href={`mailto:${CONTACT_EMAIL}`} style={{ color: 'var(--primary)' }}>{CONTACT_EMAIL}</a>. We will get back to you as soon as possible!</p>
-            <p>You may find our <a href='/terms' style={{ color: 'var(--primary)' }}>terms of service</a> and <a href='/privacy-policy' style={{ color: 'var(--primary)' }}>privacy policy</a> pages useful.</p>
+            <p>You may find our <a href='/terms' style={{ color: 'var(--primary)' }}>terms of service</a> and <a href='/privacy-policy' style={{ color: 'var(--primary)' }}>privacy policy</a> pages helpful.</p>
         </div>
     );
 }
