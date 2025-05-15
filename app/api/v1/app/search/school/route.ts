f0f85f44d78b91d02e98ca16f861b31a7037e8b8
GSDV <sdvgab@gmail.com>
2025-05-15 13:31:18 -0500

    University search

---
+++ app/api/v1/app/search/school/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest } from 'next/server';
+
+import { Prisma } from '@prisma/client';
+
+import { getValidatedUser } from '@util/prisma/actions/user';
+import { fetchClientUniverisitiesBatch } from '@util/prisma/actions/universities';
+
+import { response } from '@util/global-server';
+
+
+
+// Used for searching through posts with a query
+export async function GET(req: NextRequest) {
+    try {
+        const { searchParams } = new URL(req.url);
+
+        const queryParam = searchParams.get('query');
+        const cursor = searchParams.get('cursor');
+        if (!queryParam || cursor === null) return response(`Missing data.`, 101);
+
+        const query = queryParam.trim().toLowerCase();
+
+        const { userPrisma, validUserResp } = await getValidatedUser();
+        if (!userPrisma) return validUserResp;
+        const loggedInUserId = userPrisma.id;
+
+
+        const where: Prisma.UniversityWhereInput = {
+            OR: [
+                { name: { contains: query, mode: 'insensitive' } },
+                { domain: { contains: query, mode: 'insensitive' } }
+            ]
+        }
+        const { clientUniversities, nextCursor, moreAvailable} = await fetchClientUniverisitiesBatch(where, cursor, loggedInUserId);
+
+        return response(`Success.`, 200, { universities: clientUniversities, nextCursor, moreAvailable });
+    } catch (err) {
+        return response(`Server error.`, 904);
+    }
+}
--- src/util/prisma/actions/universities.ts
+++ src/util/prisma/actions/universities.ts
@@ -4,6 +4,8 @@ import { Prisma } from '@prisma/client';
 
 import { prisma } from '@util/prisma/client';
 
+import { PROFILE_PER_SCROLL } from '@util/global-server';
+
 
 
 export const getUni = async (where: Prisma.UniversityWhereUniqueInput) => {
@@ -59,4 +61,25 @@ export const deleteUni = async (where: Prisma.UniversityWhereUniqueInput) => {
         }
     });
     return uniPrisma;
+}
+
+
+
+export const fetchClientUniverisitiesBatch = async (where: Prisma.UniversityWhereInput, cursor: string, loggedInUserId: string) => {
+    const clientUniversities = await prisma.university.findMany({
+        where,
+        orderBy: { createdAt: 'desc' },
+        take: PROFILE_PER_SCROLL + 1,
+        ...(cursor && {
+            cursor: { id: cursor },
+            skip: 1,
+        })
+    });
+
+    const nextCursor = (clientUniversities.length!=0) ? clientUniversities[clientUniversities.length - 1].id : '';
+
+    const moreAvailable = clientUniversities.length > PROFILE_PER_SCROLL;
+    if (moreAvailable) clientUniversities.pop();
+
+    return { clientUniversities, nextCursor, moreAvailable };
 }
